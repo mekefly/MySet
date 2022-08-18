51d991b8544c46cbf8268ec5589bcbdc05cfd39c
meke <3197801970@qq.com>
2022-08-18 13:53:44 +0800

    feat(all): mySet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
-export function add(x, y) {
-  return x + y;
+export class MySet<T> implements Set<T> {
+  map: Map<T, T> = new Map();
+
+  constructor(iterable: Iterable<T>);
+  constructor();
+  constructor(iterable: null);
+  constructor(iterable?: null | Iterable<T>) {
+    if (!iterable) {
+      return;
+    }
+    for (const value of iterable) {
+      this.add(value);
+    }
+  }
+  public get size(): number {
+    return this.map.size;
+  }
+  add(value: T): this {
+    this.map.set(value, value);
+    return this;
+  }
+  clear(): void {
+    this.map.clear();
+  }
+  delete(value: T): boolean {
+    return this.map.delete(value);
+  }
+  forEach(
+    callbackfn: (value: T, value2: T, set: Set<T>) => void,
+    thisArg?: any
+  ): void {
+    this.map.forEach((value, key, map) => {
+      callbackfn(key, key, this);
+    }, thisArg);
+  }
+  has(value: T): boolean {
+    return !!this.map.get(value);
+  }
+  entries(): IterableIterator<[T, T]> {
+    return new SetEntries(this.map);
+  }
+  keys(): IterableIterator<T> {
+    return this.map.keys();
+  }
+  values(): IterableIterator<T> {
+    return this.map.keys();
+  }
+  [Symbol.iterator](): IterableIterator<T> {
+    return this.keys();
+  }
+  [Symbol.toStringTag]: string = "Set";
+}
+class SetEntries<T> implements IterableIterator<[T, T]> {
+  private mapEntries: IterableIterator<[T, T]>;
+  constructor(map: Map<T, T>) {
+    this.mapEntries = map.entries();
+  }
+  [Symbol.iterator](): IterableIterator<[T, T]> {
+    return this.mapEntries;
+  }
+  next(...args: [] | [undefined]): IteratorResult<[T, T], any> {
+    return this.mapEntries.next(...args);
+  }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,138 @@
-import { add } from "../src";
-test("first test", () => {
-  expect(1 + 1).toBe(2);
+import { MySet } from "../src";
+test("constructor", () => {
+  const set = new MySet([1, 2, 3]);
+
+  expect(set.has(1)).toBe(true);
+});
+test("size", () => {
+  new Set();
+  const set1 = new MySet();
+  const object1 = {};
+
+  set1.add(42);
+  set1.add("forty two");
+  set1.add("forty two");
+  set1.add(object1);
+
+  expect(set1.size).toBe(3);
 });
 //按F5调试错误
 test("add", () => {
-  expect(add(1, 1)).toBe(3);
+  const set = new MySet<number>();
+  set.add(1);
+  set.add(2);
+  set.add(1);
+
+  expect(set.has(1)).toBe(true);
+  expect(set.has(2)).toBe(true);
+});
+test("clear", () => {
+  const set = new MySet();
+  set.add(1);
+  set.add(2);
+  set.add(1);
+
+  set.clear();
+
+  expect(set.has(1)).toBe(false);
+  expect(set.has(2)).toBe(false);
+});
+test("delete", () => {
+  const set = new MySet();
+  set.add(1);
+  set.add(2);
+
+  set.delete(1);
+
+  expect(set.has(1)).toBe(false);
+  expect(set.has(2)).toBe(true);
+});
+test("foreach", () => {
+  const vs1 = [1, 2, 2, 3, 2];
+  const vs2 = [1, 2, 3];
+  const set = new MySet(vs1);
+  let index = 0;
+  set.forEach((value, value1, set) => {
+    if (index === 0) {
+      expect(set).toBe(set);
+    }
+    expect(value).toBe(vs2[index]);
+    expect(value1).toBe(vs2[index]);
+    index++;
+  });
+});
+test("has", () => {
+  const set = new MySet([1, 2, 3]);
+  expect(set.has(0)).toBe(false);
+  expect(set.has(1)).toBe(true);
+});
+test("entries", () => {
+  const vs1 = [1, 2];
+  const mySet = new MySet(vs1);
+  const entriesValue = mySet.entries();
+
+  expect(entriesValue.next()).toStrictEqual({
+    value: [1, 1],
+    done: false,
+  });
+  expect(entriesValue.next()).toStrictEqual({
+    value: [2, 2],
+    done: false,
+  });
+  expect(entriesValue.next()).toStrictEqual({
+    value: undefined,
+    done: true,
+  });
+  expect(entriesValue.next()).toStrictEqual({
+    value: undefined,
+    done: true,
+  });
+});
+
+test("keys", () => {
+  const vs1 = [1, 2];
+  const mySet = new MySet(vs1);
+  const keysResult = mySet.keys();
+
+  expect(keysResult.next()).toStrictEqual({
+    value: 1,
+    done: false,
+  });
+  expect(keysResult.next()).toStrictEqual({
+    value: 2,
+    done: false,
+  });
+  expect(keysResult.next()).toStrictEqual({
+    value: undefined,
+    done: true,
+  });
+});
+test("values", () => {
+  const vs1 = [1, 2];
+  const mySet = new MySet(vs1);
+  const valuesResult = mySet.values();
+
+  expect(valuesResult.next()).toStrictEqual({
+    value: 1,
+    done: false,
+  });
+  expect(valuesResult.next()).toStrictEqual({
+    value: 2,
+    done: false,
+  });
+  expect(valuesResult.next()).toStrictEqual({
+    value: undefined,
+    done: true,
+  });
+});
+test("iterator", () => {
+  const vs1 = ["1", "2", 1, 2, "1"];
+
+  const mySet = new MySet(vs1);
+
+  const exVs2 = ["1", "2", 1, 2];
+  expect([...mySet]).toStrictEqual(exVs2);
+});
+test("stringToTag", () => {
+  expect(new MySet().toString()).toBe(new Set().toString());
 });
